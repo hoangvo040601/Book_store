05f6737df26dd1f104e142d9d10a83e5bd59b40f
hoang vo <hoangvo040601@gmail.com>
2023-07-07 23:32:20 +0700

    set up React Router and Layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
